aa4c63c50aabfc73f886c1531534b0f278ae2968
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2022-07-29 19:42:38 +0530

    Config Server up and running!

---
+++ src/test/java/com/code22/configserverbom/ConfigserverBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code22.configserverbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
